b7c5efbeed3267a7a3079e2acdaec88d5f831883
Błażej <blazej.ucin213@gmail.com>
2021-11-01 15:53:15 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Quiz2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pytanie pytanie1 = new Pytanie();
+            Pytanie pytanie2 = new Pytanie();
+            Pytanie pytanie3 = new Pytanie();
+            Pytanie pytanie4 = new Pytanie();
+
+
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Pytanie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz2021
+{
+    class Pytanie
+    {
+        public int ID { get; set; }
+        public int  Kategoria { get; set; }
+        public  string Treść { get; set; }
+        public string Odp1 { get; set; }
+        public string Odp2 { get; set; }
+        public string Odp3 { get; set; }
+        public string Odp4 { get; set; }
+
+
+
+
+    }
+}
